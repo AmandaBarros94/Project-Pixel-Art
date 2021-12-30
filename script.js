5b28a970cd650a9bd45b95d0446c7203d72f1d6c
AmandaBarros94 <mandy.bjj1@gmail.com>
2021-12-29 23:44:07 -0300

    criei a paleta de cores

---
+++ script.js
@@ -0,0 +1,14 @@
+const cor1 = document.getElementsByClassName('color')[0];
+const cor2 = document.getElementsByClassName('color')[1];
+const cor3 = document.getElementsByClassName('color')[2];
+const cor4 = document.getElementsByClassName('color')[3];
+
+function adicionandCores() {
+  cor1.style.backgroundColor = '#516520';
+  cor2.style.backgroundColor = '#999520';
+  cor3.style.backgroundColor = '#513520';
+  cor4.style.backgroundColor = '#899999';
+}
+adicionandCores();
+
+
