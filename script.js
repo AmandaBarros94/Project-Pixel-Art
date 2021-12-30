02c783216624e06c83f97c4e18e6c1cfabb3594b
AmandaBarros94 <mandy.bjj1@gmail.com>
2021-12-29 23:45:48 -0300

    troquei a cor do primeiro quadrado da paleta de cores por pela cor preta

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const cor3 = document.getElementsByClassName('color')[2];
 const cor4 = document.getElementsByClassName('color')[3];
 
 function adicionandCores() {
-  cor1.style.backgroundColor = '#516520';
+  cor1.style.backgroundColor = '#000000';
   cor2.style.backgroundColor = '#999520';
   cor3.style.backgroundColor = '#513520';
   cor4.style.backgroundColor = '#899999';
